82149bb54f0ec5bba94ffb136223b165f579b7ca
mafeixiong <18060847817@163.com>
2019-11-30 14:26:11 +0800

    引入parcel，增加button组件，修改ignore

---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="v-button">
+        <slot></slot>
+    </button>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+.v-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+        border-color: var(--border-color-hover);
+    }
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+    &:focus {
+        outline: none;
+    }
+}
+
+</style>
