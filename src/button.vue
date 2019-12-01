cf0913648f5ea65236b2554c4a3a4c7397088ddd
mafeixiong <18060847817@163.com>
2019-12-01 17:20:27 +0800

    为button增加icon图标

--- src/button.vue
+++ src/button.vue
@@ -1,28 +1,41 @@
 <template>
-    <button class="v-button">
-        <slot></slot>
+    <button class="v-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
     </button>
 </template>
 <script>
-
-</script>
-<style lang="scss">
-.v-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    &:hover {
-        border-color: var(--border-color-hover);
-    }
-    &:active {
-        background-color: var(--button-active-bg);
-    }
-    &:focus {
-        outline: none;
+export default {
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: 'left',
+            validator (value) {
+                return value === 'left' || value === 'right'
+            }
+        }
     }
 }
-
+</script>
+<style lang="scss">
+.v-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
+    border-radius: var(--border-radius); border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex; justify-content: center; align-items: center;
+    vertical-align: middle;
+    &:hover { border-color: var(--border-color-hover); }
+    &:active { background-color: var(--button-active-bg); }
+    &:focus { outline: none; }
+    > .content { order: 2; }
+    > .icon { order: 1; margin-right: .1em; }
+    &.icon-right {
+        > .content { order: 1; }
+        > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+    }
+}
 </style>
