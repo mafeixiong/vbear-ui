ef0fefc71eef2d73ba7b9dba6841a75c873cd6b2
mafeixiong <18060847817@163.com>
2020-01-06 16:56:28 +0800

    实现 position的top/bottom/middle 功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="toast" ref="wrapper">
+    <div class="toast" ref="wrapper" :class="toastClasses">
         <div class="message">
             <slot v-if="!enableHtml"></slot>
             <div v-else v-html="$slots.default[0]"></div>
@@ -21,7 +21,7 @@
             },
             delay: {
                 type: [Number, String],
-                default: 5
+                default: 115
             },
             closeButton: {
                 type: Object,
@@ -34,6 +34,20 @@
             enableHtml: {
                 type: Boolean,
                 default: false
+            },
+            position: {
+                type: String,
+                default: 'top',
+                validator (value) {
+                    return ['top', 'bottom', 'middle'].includes(value)
+                }
+            }
+        },
+        computed: {
+            toastClasses() {
+                return {
+                    [`position-${this.position}`] : true
+                }
             }
         },
         mounted() {
@@ -73,9 +87,10 @@
     $toast-bg: rgba(0, 0, 0, 0.75);
     .toast {
         font-size: $font-size; min-height: $toast-min-height; line-height: 1.8;
-        position: fixed; top: 0; left: 50%; transform: translateX(-50%); display: flex;
+        position: fixed; display: flex;
         color: white; align-items: center; background: $toast-bg; border-radius: 4px;
         box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50); padding: 0 16px;
+        left: 50%;
         .message {
             padding: 8px 0;
         }
@@ -87,6 +102,18 @@
             height: 100%;
             border-left: 1px solid #666;
             margin-left: 16px;
+        }
+        &.position-top{
+            top: 0;
+            transform: translateX(-50%);
+        }
+        &.position-bottom{
+            bottom: 0;
+            transform: translateX(-50%);
+        }
+        &.position-middle{
+            top: 50%;
+            transform: translate(-50%, -50%);
         }
     }
 </style>
