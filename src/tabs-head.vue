b9a7824df6691ce0104f95cba1dcaa28dd02130e
mafeixiong <18060847817@163.com>
2020-01-09 15:08:15 +0800

    添加tabs组件

---
+++ src/tabs-head.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tabs-head">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vTabsHead"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .tabs-head{
+        display: flex;
+    }
+</style>
---
+++ src/tabs-item.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="tabs-item">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vTabsItem",
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
