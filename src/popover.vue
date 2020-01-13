4ef2ebe9c13656f63bd6d0e77c1d661a9f8dd240
mafeixiong <18060847817@163.com>
2020-01-13 16:29:07 +0800

    popover解决抖动bug 添加插槽的 close API

--- src/popover.vue
+++ src/popover.vue
@@ -4,7 +4,7 @@
              class="content-wrapper"
              :class=`position-${position}`
              v-if="visible">
-            <slot name="content"></slot>
+            <slot name="content" :close="close"></slot>
         </div>
         <span ref="triggerWrapper" class="trigger">
             <slot></slot>
@@ -150,10 +150,12 @@
             }
             &::before {
                 border-top-color: black;
+                border-bottom: none;
                 top: 100%;
             }
             &::after {
                 border-top-color: white;
+                border-bottom: none;
                 top: calc(100% - 1px);
             }
         }
@@ -163,10 +165,12 @@
                 left: 10px;
             }
             &::before {
+                border-top: none;
                 border-bottom-color: black;
                 bottom: 100%;
             }
             &::after {
+                border-top: none;
                 border-bottom-color: white;
                 bottom: calc(100% - 1px);
             }
@@ -180,10 +184,12 @@
             }
             &::before {
                 border-left-color: black;
+                border-right: none;
                 left: 100%;
             }
             &::after {
                 border-left-color: white;
+                border-right: none;
                 left: calc(100% - 1px);
             }
         }
@@ -195,10 +201,12 @@
             }
             &::before {
                 border-right-color: black;
+                border-left: none;
                 right: 100%;
             }
             &::after {
                 border-right-color: white;
+                border-left: none;
                 right: calc(100% - 1px);
             }
         }
