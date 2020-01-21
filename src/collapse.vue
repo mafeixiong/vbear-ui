6d68c6110e573f6de97de98662260f330971ea77
mafeixiong <18060847817@163.com>
2020-01-21 11:22:45 +0800

    完成collapse功能

--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,50 @@
 </template>
 
 <script>
+    import Vue from 'vue'
+
     export default {
-        name: "vCollapse"
+        name: "vCollapse",
+        props: {
+            single: {
+                type: Boolean,
+                default: false
+            },
+            selected: {
+                type: Array,
+            }
+        },
+        data() {
+            return {
+                eventBus: new Vue()
+            }
+        },
+        provide() {
+            return {
+                eventBus: this.eventBus
+            }
+        },
+        mounted() {
+            this.$emit('update:selected', this.selected)
+            this.eventBus.$on('update:addSelected', (name) => {
+                let selectCopy = JSON.parse(JSON.stringify(this.selected))
+                if (this.single) {
+                    selectCopy = [name]
+                } else {
+                    selectCopy.push(name)
+                }
+                this.eventBus.$emit('update:selected', selectCopy)
+                this.$emit('update:selected', selectCopy)
+                console.log(selectCopy)
+            })
+            this.eventBus.$on('update:removeSelected', (name) => {
+                let selectCopy = JSON.parse(JSON.stringify(this.selected))
+                let index = selectCopy.indexOf(name)
+                selectCopy.splice(index, 1)
+                this.eventBus.$emit('update:selected', selectCopy)
+                this.$emit('update:selected', selectCopy)
+            })
+        }
     }
 </script>
 
