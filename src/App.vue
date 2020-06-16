db0dbaabcbf783eaaeec85c2bf26563fae93fa3d
马飞雄 <770520615@qq.com>
2020-06-16 13:20:36 +0800

    添加 nav组件

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,51 @@
 <template>
-    <div style="display: flex; justify-content: center;">
-        <div style="width: 600px; border: 1px solid black;">
-            <p>段落1</p>
-            <p>段落2</p>
-            <p>段落3</p>
-            <p>段落4</p>
-            <p>段落5</p>
-            <p>段落6</p>
-            <p>段落7</p>
-            <p>段落8</p>
-            <p>段落9</p>
-            <v-sticky :distance="100">
-                <div style="border: 1px solid red; ">
-                    <img src="https://i.loli.net/2020/04/27/UcBeGxRwXZESiI2.png" />
-                </div>
-            </v-sticky>
-            <p>段落1</p>
-            <p>段落2</p>
-            <p>段落3</p>
-            <p>段落4</p>
-            <p>段落5</p>
-            <p>段落6</p>
-            <p>段落7</p>
-            <p>段落8</p>
-            <p>段落9</p>
-            <p>段落10</p>
-            <p>段落11</p>
-            <p>段落12</p>
-            <p>段落13</p>
-            <p>段落14</p>
-            <p>段落15</p>
-            <p>段落16</p>
-            <p>段落17</p>
-            <p>段落18</p>
-            <p>段落19</p>
-            <p>段落20</p>
-        </div>
-    </div>
-
+  <div>
+    <v-nav :selected.sync="selected">
+      <v-nav-item name="home">首页</v-nav-item>
+      <v-nav-item name="about">
+        <v-nav-sub>
+          <template slot="title">关于</template>
+          <v-nav-item name="about1">公司文化</v-nav-item>
+          <v-nav-item name="about2">企业简介</v-nav-item>
+          <v-nav-item name="about3">联系我们</v-nav-item>
+          <!--                    <v-nav-sub>-->
+          <!--                        <template slot="title">联系方式</template>-->
+          <!--                        <v-nav-item name="wechat">微信</v-nav-item>-->
+          <!--                        <v-nav-item name="qq">QQ</v-nav-item>-->
+          <!--                        <v-nav-sub>-->
+          <!--                            <template slot="title">手机</template>-->
+          <!--                            <v-nav-item name="cm">移动</v-nav-item>-->
+          <!--                            <v-nav-item name="cu">联通</v-nav-item>-->
+          <!--                            <v-nav-item name="cn">电信</v-nav-item>-->
+          <!--                        </v-nav-sub>-->
+          <!--                    </v-nav-sub>-->
+        </v-nav-sub>
+      </v-nav-item>
+      <v-nav-item name="job">招聘</v-nav-item>
+    </v-nav>
+    <v-button>按钮</v-button>
+  </div>
 </template>
 
 <script>
-  import vSticky from './sticky/sticky'
-
-  export default {
-    name: 'demo',
-    components: {
-      vSticky
-    }
+import vNav from "./nav/nav";
+import vNavItem from "./nav/nav-item";
+import vNavSub from "./nav/nav-sub";
+import vButton from "./button/button";
+export default {
+  name: "demo",
+  components: {
+    vNav,
+    vNavItem,
+    vNavSub,
+    vButton
+  },
+  data() {
+    return {
+      selected: ["about2"]
+    };
   }
+};
 </script>
 <style>
-    img {max-width: 100%;}
 </style>
---
+++ src/nav/nav-sub.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="v-nav-sub">
+        <span @click="onClick">
+            <slot name="title"></slot>
+        </span>
+        <div class="v-nav-sub-popover" v-show="open">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'vNavSub',
+    data() {
+      return {
+        open: false
+      }
+    },
+    methods: {
+      onClick() {
+        this.open = !this.open
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.v-nav-sub {
+    position: relative;
+    > span {
+        padding: 10px 20px;
+        display: block;
+    }
+    &-popover {
+        position: absolute;
+        top: 100%;
+        left: 0;
+        border: 1px solid black;
+        white-space: nowrap;
+    }
+}
+</style>
